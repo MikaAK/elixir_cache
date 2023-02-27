3586c17c61b91cceeb1814596b0b033b92e4423e
Jack Howells <info@jhowells.com>
2023-02-27 01:53:07 -0800

    Fix/redis adapter hash fixes (#5)
    * Write failing tests for hash-based redis operations
    
    * Ensure hash fields are also binary encoded/decoded in redis ops
    
    * Avoids repeated pool name causing flaky async test results
    
    * Start the sandbox agent, not the Cache.Agent, when using the sandbox adapter
    
    * Correct test arity notation
    
    ---------
    
    Co-authored-by: Mika Kalathil <mika@kalathil.me>

--- lib/cache/redis.ex
+++ lib/cache/redis.ex
@@ -101,7 +101,9 @@ defmodule Cache.Redis do
 
   @impl Cache
   def put(pool_name, key, ttl, value, opts \\ []) do
-    command(pool_name, redis_set_command(pool_name, key, ttl, value), opts)
+    with {:ok, "OK"} <- command(pool_name, redis_set_command(pool_name, key, ttl, value), opts) do
+      :ok
+    end
   end
 
   defp redis_set_command(pool_name, key, _ttl, nil) do
@@ -135,6 +137,8 @@ defmodule Cache.Redis do
   end
 
   def hash_get(pool_name, key, field, opts) do
+    field = TermEncoder.encode(field, opts[:compression_level])
+
     with {:ok, value} when not is_nil(value) <-
            command(pool_name, ["HGET", cache_key(pool_name, key), field], opts) do
       {:ok, TermEncoder.decode(value)}
@@ -146,8 +150,8 @@ defmodule Cache.Redis do
       hash =
         data
         |> Enum.chunk_every(2)
-        |> Map.new(fn [key, value] ->
-          {TermEncoder.decode(key), TermEncoder.decode(value)}
+        |> Map.new(fn [field, value] ->
+          {TermEncoder.decode(field), TermEncoder.decode(value)}
         end)
 
       {:ok, hash}
@@ -155,6 +159,7 @@ defmodule Cache.Redis do
   end
 
   def hash_set(pool_name, key, field, value, opts) do
+    field = TermEncoder.encode(field, opts[:compression_level])
     value = TermEncoder.encode(value, opts[:compression_level])
 
     command(pool_name, ["HSET", cache_key(pool_name, key), field, value], opts)
@@ -162,13 +167,18 @@ defmodule Cache.Redis do
 
   def hash_set_many(pool_name, key_values, ttl, opts) do
     commands =
-      Enum.map(key_values, fn {key, key_values} ->
-        key_values =
-          Enum.map(key_values, fn {key, value} ->
-            {key, TermEncoder.encode(value, opts[:compression_level])}
+      Enum.map(key_values, fn {key, field_values} ->
+        field_values =
+          field_values
+          |> Enum.map(fn {field, value} ->
+            [
+              TermEncoder.encode(field, opts[:compression_level]),
+              TermEncoder.encode(value, opts[:compression_level])
+            ]
           end)
+          |> List.flatten()
 
-        ["HSET", cache_key(pool_name, key) | key_values]
+        ["HSET", cache_key(pool_name, key) | field_values]
       end)
 
     expiries =
@@ -184,11 +194,19 @@ defmodule Cache.Redis do
   end
 
   def hash_delete(pool_name, key, field, opts) do
+    field = TermEncoder.encode(field, opts[:compression_level])
     command(pool_name, ["HDEL", cache_key(pool_name, key), field], opts)
   end
 
   def hash_values(pool_name, key, opts) do
-    command(pool_name, ["HVALS", cache_key(pool_name, key)], opts)
+    with {:ok, data} <- command(pool_name, ["HVALS", cache_key(pool_name, key)], opts) do
+      values =
+        Enum.map(data, fn value ->
+          TermEncoder.decode(value)
+        end)
+
+      {:ok, values}
+    end
   end
 
   defp cache_key(pool_name, key) do
