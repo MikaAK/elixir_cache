8f0172f3c13ecaef170d551b9bc607b4d9087d6e
MikaAK <mika@kalathil.me>
2025-03-07 23:43:24 -0800

    chore: add prom telemetry

--- mix.exs
+++ mix.exs
@@ -49,6 +49,7 @@ defmodule ElixirCache.MixProject do
 
       {:telemetry, "~> 1.1"},
       {:telemetry_metrics, "~> 1.0"},
+      {:prometheus_telemetry, "~> 0.3", optional:  true},
 
       {:faker, "~> 0.17", only: [:test]},
       {:credo, "~> 1.6", only: [:test, :dev], runtime: false},
